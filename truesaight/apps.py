8efc9c1806b1dde35cca6482344b27eedda6f5df
jqb <jkm.kiunisala@gmail.com>
2025-04-09 00:37:34 +0800

    chore: initialized app files

--- truesaight/apps.py
+++ truesaight/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DeepfakedectectorConfig(AppConfig):
+class TruesaightConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'deepfakedectector'
+    name = 'truesaight'
---
+++ truesaight/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'truesaight'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    # path('result/', views.result, name='result'),
+
+]
