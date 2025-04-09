a90f590dce9ba4fe85641969fd9677340f54a5c6
b0kja85 <jkm.kiunisala@gmail.com>
2025-04-09 17:51:15 +0800

    refactor(forms): refactored forms to enhance the UI

--- truesaight/forms.py
+++ truesaight/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
 class VideoUploadForm(forms.Form):
-    video_file= forms.FileField(label='Select a Video File')
+    video_file = forms.FileField(
+        label='Select a Video File',
+        widget=forms.ClearableFileInput(attrs={
+            'class': 'form-control',
+            'id': 'videoFileInput',
+            'accept': 'video/*'
+        })
+    )
