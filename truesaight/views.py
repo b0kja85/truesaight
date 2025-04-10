205430d5448561515783ad67409b452b8612e2ec
b0kja85 <jkm.kiunisala@gmail.com>
2025-04-10 16:22:25 +0800

    feat(view/controller): finalized controller

--- truesaight/views.py
+++ truesaight/views.py
@@ -1,37 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import VideoUploadForm
 from .models import VideoProcessing
 from .utils import process_video_task
+from threading import Thread
 
 def home(request):
     form = VideoUploadForm()
-    return render(request,'truesaight/home.html', {'form': form})
+    return render(request, 'truesaight/home.html', {'form': form})
 
 def upload_video(request):
     if request.method == 'POST':
         form = VideoUploadForm(request.POST, request.FILES)
         
         if form.is_valid():
-            # 1. Pull the UploaddedFile out of the Form
             video_file = form.cleaned_data['video_file']
 
-            # 2. Make a new instance of the Model for 
-            # data initialization into the DB
             video_task = VideoProcessing.objects.create(
                 video_file=video_file,
                 status='queued',
                 confidence_score=0.0,
                 processed_frames_count=0
             )
-            
-            # 3. Call the Service & Change Status to Processing
-            process_video_task(video_task)
 
-            return render(request, 'truesaight/result.html', {'video_name': video_file.name})
+            # Kick off async background processing
+            Thread(target=process_video_task, args=(video_task.id,)).start()
+
+            # Redirect to spinner while processing
+            return redirect('truesaight:processing', pk=video_task.id)
     else:
         form = VideoUploadForm()
 
     return render(request, 'truesaight/upload.html', {'form': form})
 
-# def result(request):
-#     return render(request, 'truesaight/result.html')
+def processing_view(request, pk):
+    video = get_object_or_404(VideoProcessing, pk=pk)
+
+    # Redirect to result when processing is done
+    if video.status == 'completed':
+        return redirect('truesaight:result', pk=video.id)
+
+    return render(request, 'truesaight/processing.html', {'video': video})
+
+def result_view(request, pk):
+    video = get_object_or_404(VideoProcessing, pk=pk)
+    return render(request, 'truesaight/result.html', {'video': video})
