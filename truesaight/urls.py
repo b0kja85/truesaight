6cf43521a943078ac5dab00e03b20712b2648a41
b0kja85 <jkm.kiunisala@gmail.com>
2025-04-10 16:23:11 +0800

    feat(urls): finalized the URLs

--- truesaight/urls.py
+++ truesaight/urls.py
@@ -4,8 +4,16 @@ from . import views
 app_name = 'truesaight'
 
 urlpatterns = [
-    path('', views.home, name='home'), # Default Home    
-    path('upload/', views.upload_video, name='upload_video')
-    # path('results/<int:pk>', views.result, name='result')
-
+    path('', views.home, name='home'),  # Homepage with Upload Form
+    path('upload/', views.upload_video, name='upload_video'),  # Handle Upload POST
+    path('processing/<int:pk>/', views.processing_view, name='processing'),  # Show loading spinner
+    path('result/<int:pk>/', views.result_view, name='result'),  # Final results
 ]
+
+# Serving the Media Files
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
